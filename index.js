4c74a1b728711e8c0acfa055255f118b38ef4612
Nelly <nellysattari@gmail.com>
2018-07-28 13:30:11 +1000

    removed unused images

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 
-
-// export class getReciepes {
-//     find() {
-//     }
-
-// }
-
 const foodCalories = [
     { "food": "iceCream", "calory": 500, },
     { "food": "roasPumkin", "calory": 300 },
     { "food": "spaghetti", "calory": 400 }
 ];
+let _this;
+  
+class getReciepes {
+    constructor() {
+        _this= this ;
+      }
+    
 
-   
-const getCalories = (foodName) => {
-    console.log(foodName);
-    let val= foodCalories.find(x => x.food === foodName).calory;
-    console.log(`Calory of ${foodName}:`,val);
-    return val;
+    static get foodCalories() {
+        return  [
+            { "food": "iceCream", "calory": 500, },
+            { "food": "roasPumkin", "calory": 300 },
+            { "food": "spaghetti", "calory": 400 }
+        ];
+      }
+    //   const handleSuccess = (result, callback) => {}
+    showcalory(){
+        let element= document.querySelectorAll('[data-foo="value"]');
+ 
+    };
+    getCalories (foodName){
+        console.log(foodName);
+        let val = foodCalories.find(x => x.food === foodName).calory;
+        console.log(`Calory of ${foodName}:`, val);
+        console.log(document.querySelectorAll(`[data-name=${foodName}]`));
+        document.querySelectorAll(`[data-name=${foodName}]`)[0].textContent=`calory is ${val}`
+        // _this.showcalory();
+    };
+ 
 };
-
-// console.log(getCalories("iceCream"));
+// console.log(new getReciepes().getCalories("iceCream"));
