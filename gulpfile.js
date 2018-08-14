4fd031bd254f30b68a90fda193a3c964c5123267
Nelly Sattari <nsattari@challenger.com.au>
2018-08-14 12:37:36 +1000

    fixed webpack

--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,34 @@ let gulp = require(`gulp`),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload,
     browserChoice = `default`;
-   
 
- 
-    gulp.task(`allBrowsers`, function () {
-        browserChoice = [`safari`, `firefox`, `google chrome`, `opera`, `microsoft-edge`];
-    });
- 
+
+
+gulp.task(`allBrowsers`, function () {
+    browserChoice = [`safari`, `firefox`, `google chrome`, `opera`, `microsoft-edge`];
+});
+
+
+gulp.task('compileSass', function () {
+    var autoPrefixerPlugins = [autoprefixerBrowserSpecific({ browsers: [`last 2 versions`] })]
+console.log( path.join(__dirname));
+    return gulp.src(`styles/main.scss`)
+        .pipe(sass({
+            outputStyle: `expanded`,
+            precision: 10,
+            includePaths: [
+                path.join(__dirname)
+            ]
+        }).on(`error`, sass.logError))
+        .pipe(postcss(autoPrefixerPlugins))
+        .pipe(gulp.dest(`styles`));
+});
 
 gulp.task(`serve`, gulp.series('compileSass', function () {
     browserSync.init({
         watch: true,
         notify: true,
-        port: 9000,
+        port: 8000,
         reloadDelay: 100,
         browser: browserChoice,
         server: {
@@ -38,23 +53,8 @@ gulp.task(`serve`, gulp.series('compileSass', function () {
     });
 
     gulp.watch('styles/**/*.scss', gulp.parallel('serve'));
-    gulp.watch('scripts/**/*.js', gulp.parallel('serve'));   
+    gulp.watch('scripts/**/*.js', gulp.parallel('serve'));
     gulp.watch('**/*.html', gulp.parallel('serve'));
-   
+
 
 }));
-     
-gulp.task('compileSass', function () {
-    var autoPrefixerPlugins = [autoprefixerBrowserSpecific({ browsers: [`last 2 versions`] })]
- 
-    return gulp.src(`styles/main.scss`)
-        .pipe(sass({
-            outputStyle: `expanded`,
-            precision: 10,
-            includePaths: [
-                path.join(__dirname)
-            ]
-        }).on(`error`, sass.logError))
-        .pipe(postcss(autoPrefixerPlugins))
-        .pipe(gulp.dest(`styles`));
-});
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,7 +12,14 @@ module.exports = (env) => {
         module: {
             rules: [
                 {
-                    test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: ['es2015'], },
+                    test: /\.js$/,
+                     exclude: /node_modules/,
+                     use: {
+                        loader: 'babel-loader',
+                        options: {
+                          presets: ['babel-preset-env']
+                        }
+                      }
                 }
             ]
         }
